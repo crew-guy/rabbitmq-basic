73a86b823f2002571c77b9dc2e6cb4fa28442625
ankit <ankit.sanghavi87@gmail.com>
2022-10-07 08:35:10 +0530

    finished boilerplate

--- starter/orders-api/index.js
+++ starter/orders-api/index.js
@@ -1,7 +1,24 @@
 const express = require("express")
 const app = express()
+const amqp = require('amqplib')
+
+const amqpUrl = "amqp://localhost:5672"
+
+const orderData = {
+    customerId: 8,
+    orderId: 7,
+    number: "222 222 3333"
+}
 
 app.get('/', async (req, res) => {
+    try {
+        const connection = await amqp.connect(amqpUrl)
+        const channel = await connection.createChannel()
+        await channel.assertQueue("order.shipped")
+        channel.sendToQueue("order.shipped", Buffer.from(JSON.stringify(orderData)))
+    } catch (error) {
+        console.log(error)        
+    }
     res.send("ORDERS API")
 })
 
